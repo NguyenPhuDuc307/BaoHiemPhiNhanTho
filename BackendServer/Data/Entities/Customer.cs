8e1b20c69f1bce4e0aca20ac82c5262915f9aa2b
Bimbb <100526119+Bimbb@users.noreply.github.com>
2023-06-05 16:32:59 +0700

    Entities

---
+++ BackendServer/Data/Entities/Customer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BaoHiemPhiNhanTho.BackendServer.Models;
+
+public class Customer
+{
+    public string? Cif { get; set; }
+    public string? Name { get; set; }
+    public string? CustomerType { get; set; }
+    public string? Gender { get; set; }
+    public string? CCCD { get; set; }
+    public ICollection<InsuranceContract>? InsuranceContracts { set; get; }
+}
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.ToTable("Customers");
+
+        builder.HasKey(x => x.Cif);
+
+        builder.Property(x => x.Cif)
+               .IsRequired()
+               .HasMaxLength(50);
+
+        builder.Property(x => x.Name)
+               .IsRequired()
+               .HasMaxLength(255);
+
+        builder.Property(x => x.CustomerType)
+               .IsRequired()
+               .HasMaxLength(255);
+
+        builder.Property(x => x.Gender)
+               .IsRequired()
+               .HasMaxLength(50);
+
+        builder.Property(x => x.CCCD)
+               .IsRequired()
+               .HasMaxLength(50);
+
+    }
+}
---
+++ BackendServer/Data/Entities/PaymentPeriod.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BaoHiemPhiNhanTho.BackendServer.Models;
+
+public class PaymentPeriod
+{
+    public string? Id { get; set; }
+    public string? TotalAmount { get; set; }
+    public string? Period { get; set; }
+    public DateTime? FeePaymentDate { get; set; }
+    public float? Money { get; set; }
+
+    public string? HDBH { get; set; }
+    public InsuranceContract? InsuranceContract { set; get; }
+}
+
+public class PaymentPeriodConfiguration : IEntityTypeConfiguration<PaymentPeriod>
+{
+    public void Configure(EntityTypeBuilder<PaymentPeriod> builder)
+    {
+        builder.ToTable("PaymentPeriods");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+               .IsRequired()
+               .HasMaxLength(50);
+
+        builder.Property(x => x.TotalAmount)
+               .IsRequired();
+
+        builder.Property(x => x.Period)
+               .IsRequired();
+
+        builder.Property(x => x.FeePaymentDate)
+               .IsRequired();
+
+        builder.Property(x => x.Money)
+               .IsRequired();
+
+        builder.HasOne(x => x.InsuranceContract)
+                   .WithMany(x => x.PaymentPeriods)
+                   .HasForeignKey(x => x.HDBH);
+    }
+}

9801e46b0abf61132a3607436ba52a7b9a1bd4ba
Nguyễn Phú Đức <95112332+NguyenPhuDuc307@users.noreply.github.com>
2023-06-05 10:28:42 +0700

    update

---
+++ BackendServer/Models/ThongTinKhachHang.cs
@@ -0,0 +1,10 @@
+namespace BackendServer.Models;
+
+public class ThongTinKhachHang
+{
+    public string? Id { get; set; }
+    public string? CustomerCategory { get; set; }
+    public string? FullName { get; set; }
+    public int Gender { get; set; }
+    public string? CCCD { get; set; }
+}
---
+++ BackendServer/Models/ThongTinTS_SPBH.cs
@@ -0,0 +1,10 @@
+namespace BackendServer.Models;
+
+public class ThongTinTS_SPBH
+{
+    public string? SoHDBH { get; set; }
+    public string? TrangThaiHD { get; set; }
+    public string? InsuranceObject { get; set; }
+    public string? Partner { get; set; }
+    public string? MaTSBD { get; set; }
+}

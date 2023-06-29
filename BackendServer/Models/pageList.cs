041b0178df188bb3fbe51f23634973a06a386df9
Bimbb <100526119+Bimbb@users.noreply.github.com>
2023-06-29 15:01:12 +0700

    Update return list

--- BackendServer/Models/pageList.cs
+++ BackendServer/Models/pageList.cs
@@ -5,15 +5,19 @@ namespace Models
 {
     public class PagedList<T>
     {
-        public List<T> Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public List<T> ResultObj { get; set; }
         public int TotalCount { get; set; }
         public int Page { get; set; }
         public int PageSize { get; set; }
         public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
 
-        public PagedList(List<T> data, int totalCount, int page, int pageSize)
+        public PagedList(bool isSuccess, string message, List<T> resultObj, int totalCount, int page, int pageSize)
         {
-            Data = data;
+            IsSuccess = isSuccess;
+            Message = message;
+            ResultObj = resultObj;
             TotalCount = totalCount;
             Page = page;
             PageSize = pageSize;

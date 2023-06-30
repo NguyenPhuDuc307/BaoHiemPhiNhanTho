0d0782cbe5878c2fea2f4e1ffef7e0551b765a7d
xuanthanh2609 <thanh26092000@gmail.com>
2023-06-30 13:52:44 +0700

    test

---
+++ backendServer.UnitTesting/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using BackendServer;
+
+namespace backendServer.UnitTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
